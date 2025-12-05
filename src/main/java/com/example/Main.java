66b030de8becc467200a4f3748a73ccd10619162
Jesper Larsson <jesper.larsson@iths.se>
2025-12-05 15:44:32 +0100

    basic login functionality
    Signed-off-by: Jesper Larsson <jesper.larsson@iths.se>

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,8 +1,6 @@
 package com.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Arrays;
 
 public class Main {
@@ -27,11 +25,58 @@ public class Main {
         }
 
         try (Connection connection = DriverManager.getConnection(jdbcUrl, dbUser, dbPass)) {
+
+            boolean authorized = login(connection);
+
+            if (authorized) {
+                promptMenu();
+            }
+            else {
+                System.out.println("Username or password is invalid.");
+                System.exit(0);
+            }
+
         } catch (SQLException e) {
             throw new RuntimeException(e);
         }
-        //Todo: Starting point for your code
+
+
+
     }
+
+
+    private boolean login(Connection connection) {
+        String unm = IO.readln("Username: ");
+        String pw = IO.readln("Password: ");
+
+        String accQuery = "select count(*) from account where binary name = ? and binary password = ?";
+        try(PreparedStatement statement = connection.prepareStatement(accQuery)){
+            statement.setString(1, unm);
+            statement.setString(2, pw);
+
+            ResultSet rs = statement.executeQuery();
+            while(rs.next()) {
+                if(rs.getInt("count(*)") == 1){return true;}
+            }
+            return false;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private int promptMenu(){
+        System.out.print(
+                "1) List moon missions (prints spacecraft names from `moon_mission`).\n" +
+                "2) Get a moon mission by mission_id (prints details for that mission).\n" +
+                "3) Count missions for a given year (prompts: year; prints the number of missions launched that year).\n" +
+                "4) Create an account (prompts: first name, last name, ssn, password; prints confirmation).\n" +
+                "5) Update an account password (prompts: user_id, new password; prints confirmation).\n" +
+                "6) Delete an account (prompts: user_id; prints confirmation).\n" +
+                "0) Exit.\n");
+
+        return Integer.parseInt(IO.readln());
+    }
+
 
     /**
      * Determines if the application is running in development mode based on system properties,
