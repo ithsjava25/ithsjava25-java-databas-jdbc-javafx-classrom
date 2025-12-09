b79984dc99d2b0633392eee3ede7ade88f57833c
Adam Ottosson <addeecsgo@gmail.com>
2025-12-09 03:52:32 +0100

    adds mission domain model for moon mission results

---
+++ src/main/java/com/example/Mission.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Mission {
+    public long missionId;
+    public String spacecraft;
+
+    public Mission(long missionId, String spacecraft) {
+        this.missionId = missionId;
+        this.spacecraft = spacecraft;
+    }
+}
