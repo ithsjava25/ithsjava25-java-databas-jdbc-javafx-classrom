9e8cb48561f83632d142b191dc08399da868ee7b
Linda Eskilsson <eskilsson.linda@hotmail.se>
2025-12-10 11:26:08 +0100

    Step 3. log in function successfully implemented

--- src/main/java/com/example/Main.java
+++ src/main/java/com/example/Main.java
@@ -1,10 +1,8 @@
 package com.example;
 
-import java.io.Console;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class
 Main {
@@ -40,28 +38,55 @@ Main {
             throw new RuntimeException("Could not connect to DB", e);
         }
 
-
         //Todo: Starting point for your code
-        Console console = System.console();
-        String uName;
-        String pWord;
+        Scanner scanner = new Scanner(System.in);
 
-        if (console != null) {
-            uName = console.readLine("Username: ");
-            char[] pwChars = console.readPassword("Password: ");
-            pWord = new String(pwChars);
-        } else {
-            // IntelliJ fallback (du är här nu!)
-            System.out.print("Username: ");
-            uName = new java.util.Scanner(System.in).nextLine();
-
-            System.out.print("Password: ");
-            pWord = new java.util.Scanner(System.in).nextLine();
+        while (!login(this.connection, scanner)) {
+            // fortsätt försöka tills login lyckas
         }
 
-        System.out.println("You entered username: " + uName);
-
+        System.out.println("Login succsessfull! let's move on to the menu...");
     }
+
+    public static boolean login(Connection conn, Scanner scanner) {
+
+        System.out.print("Username (or 0 to exit): ");
+        String username = scanner.nextLine();
+
+        if (username.equals("0")) {
+            System.out.println("Exiting...");
+            System.exit(0);
+        }
+
+        System.out.print("Password (or 0 to exit): ");
+        String password = scanner.nextLine();
+
+        if (password.equals("0")) {
+            System.out.println("Exiting...");
+            System.exit(0);
+        }
+
+        String sql = "SELECT * FROM account WHERE first_name = ? AND password = ?";
+
+        try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+            stmt.setString(1, username);
+            stmt.setString(2, password);
+
+            ResultSet rs = stmt.executeQuery();
+
+            if (!rs.next()) {
+                System.out.println("Invalid credentials");
+                return false;
+            }
+
+            System.out.println("Login successful!");
+            return true;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("Login failed", e);
+        }
+    }
+
 
     /**
      * Determines if the application is running in development mode based on system properties,
