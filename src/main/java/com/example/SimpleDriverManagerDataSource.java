9d260275a0d81785a21ef4f4ce0f141f3d3cb5ce
Kristina M <kristina0x7@gmail.com>
2025-12-11 12:02:32 +0100

    update main and SimpleDriverManagerDataSource class

--- src/main/java/com/example/SimpleDriverManagerDataSource.java
+++ src/main/java/com/example/SimpleDriverManagerDataSource.java
@@ -1,4 +1,32 @@
 package com.example;
 
-public class SimpleDriverManagerDataSource {
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SimpleDriverManagerDataSource implements DataSource {
+    private final String url;
+    private final String username;
+    private final String password;
+
+    public SimpleDriverManagerDataSource(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public Connection getConnection() throws SQLException { return DriverManager.getConnection(url, username, password); }
+
+    @Override
+    public Connection getConnection(String username, String password) throws SQLException { return DriverManager.getConnection(url, username, password); }
+
+    @Override public <T> T unwrap(Class<T> iface) { throw new UnsupportedOperationException(); }
+    @Override public boolean isWrapperFor(Class<?> iface) { return false; }
+    @Override public java.io.PrintWriter getLogWriter() { throw new UnsupportedOperationException(); }
+    @Override public void setLogWriter(java.io.PrintWriter out) { throw new UnsupportedOperationException(); }
+    @Override public void setLoginTimeout(int seconds) { throw new UnsupportedOperationException(); }
+    @Override public int getLoginTimeout() { return 0; }
+    @Override public java.util.logging.Logger getParentLogger() { throw new UnsupportedOperationException(); }
 }
