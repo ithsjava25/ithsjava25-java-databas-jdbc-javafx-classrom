75105b4e9c7e6cfddf2cd86520d26a8723886fc8
Kristina M <kristina0x7@gmail.com>
2025-12-11 11:24:40 +0100

    move method readline from main: to input reader class

--- src/main/java/com/example/cli/InputReader.java
+++ src/main/java/com/example/cli/InputReader.java
@@ -1,4 +1,19 @@
 package com.example.cli;
 
 public class InputReader {
+
+
+    // Reads a line from System.in using Java 25 IO
+    private static String readLine(InputStream in) throws IOException {
+        byte[] buffer = new byte[1024];
+        int len = 0;
+        int b;
+        while ((b = in.read()) != -1) {
+            if (b == '\n') break;
+            buffer[len++] = (byte) b;
+        }
+        return new String(buffer, 0, len).trim();
+    }
+
+
 }
